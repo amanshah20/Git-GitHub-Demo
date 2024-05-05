be6ffd86534e2dc46a8dea1e73d412f96399168c
aman kumar kanu <118723033+amanshah20@users.noreply.github.com>
2024-05-05 23:14:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('changeText').addEventListener('click', function() {
+    document.getElementById('content').innerHTML = '<p>Text changed!</p>';
+});
+// Add JavaScript functionality here
+console.log("Script loaded!");
+
+// Example console output
+console.log("This is a console log message.");
